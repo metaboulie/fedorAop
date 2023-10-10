65b62216964fbabcf2eae9ccbe47572a0fb39491
metaboulie <eugeneheiner14@gmail.com>
2023-10-10 19:09:05 +0800

    Add test for io.py, delete polars methods

---
+++ fedorAop/io.py
@@ -0,0 +1,74 @@
+import os
+
+import anndata as ad
+import numpy as np
+from sklearn.preprocessing import LabelEncoder
+
+
+def read_h5ad_datasets(directory: str) -> dict[str, ad.AnnData]:
+    """Read all datasets inside a given folder and use a dictionary to store them
+
+    Parameters
+    ----------
+    directory : str
+        The directory where the Dataset folder is located
+
+    Returns
+    -------
+    dict[str, ad.AnnData]
+        A dictionary whose keys being the name of each dataset, values being the data of each dataset
+    """
+    return {
+        os.path.splitext(file)[0]: ad.read_h5ad(os.path.join(root, file))
+        for root, _, files in os.walk(directory)
+        for file in files
+        if file.endswith(".h5ad")
+    }
+
+
+def convert_dataset_dict_to_np_dict(
+    dataset_dict: dict[str, ad.AnnData]
+) -> dict[str, np.ndarray]:
+    """
+    Convert each dataset in the given dictionary from `ad.AnnData` to `np.ndarray`,
+    encode the 'cell_type' stored in `adata.obs['cell.type']` as numeric labels,
+    and append the labels to the data.
+
+    Parameters
+    ----------
+    dataset_dict : dict[str, ad.AnnData]
+        A dictionary whose keys are the names of each dataset and values are the data of each dataset.
+
+    Returns
+    -------
+    dict[str, np.ndarray]
+         A dictionary transformed from the input dictionary, with the last column encoded from the 'cell_type' of each observation.
+    """
+    np_dict = {}
+
+    for dataset_name, adata in dataset_dict.items():
+        # Initialize label encoder
+        label_encoder = LabelEncoder()
+
+        # Encode 'cell_type' to numeric labels
+        encoded_values = label_encoder.fit_transform(adata.obs["cell.type"])
+
+        # Append encoded values to the data
+        np_dict[dataset_name] = np.concatenate(
+            (np.array(adata.X), encoded_values[:, None]), axis=1
+        )
+
+    # Sort and return the transformed dictionary
+    return dict(sorted(np_dict.items()))
+
+
+def get_data_dict(directory: str) -> dict[str, np.ndarray]:
+    """
+    Generates a dictionary of data arrays from the datasets stored in the specified directory.
+
+    :param directory: The directory where the datasets are stored.
+    :type directory: str
+    :return: A dictionary mapping dataset names to NumPy arrays.
+    :rtype: dict[str, np.ndarray]
+    """
+    return convert_dataset_dict_to_np_dict(read_h5ad_datasets(directory))
