71eb99fbba250b632c3692978196f781b8298eb3
metaboulie <eugeneheiner14@gmail.com>
2023-11-05 14:29:20 +0800

    Update io in utils package and regarding tests

--- fedorAop/utils/io.py
+++ fedorAop/utils/io.py
@@ -3,6 +3,7 @@ import os
 import anndata as ad
 import numpy as np
 from sklearn.preprocessing import LabelEncoder
+import json
 
 
 def read_h5ad_datasets(directory: str) -> dict[str, ad.AnnData]:
@@ -26,9 +27,27 @@ def read_h5ad_datasets(directory: str) -> dict[str, ad.AnnData]:
     }
 
 
-def convert_dataset_dict_to_np_dict(
-    dataset_dict: dict[str, ad.AnnData]
-) -> dict[str, np.ndarray]:
+def h5ad_to_json(directory: str, cell_type_key: str) -> None:
+    """Read .h5ad files and write their data, including cell type values, to individual JSON files.
+
+    Parameters
+    ----------
+    directory : str
+        The directory where the .h5ad files are located.
+    cell_type_key : str
+        The key to access the cell type information in the AnnData object.
+    """
+    datasets = read_h5ad_datasets(directory)
+
+    for dataset_name, adata in datasets.items():
+        data_dict = {"X": adata.X.tolist(), "obs": {cell_type_key: adata.obs[cell_type_key].tolist()}}
+
+        output_file = f"{dataset_name}.json"
+        with open(output_file, "w") as json_file:
+            json.dump(data_dict, json_file)
+
+
+def convert_dataset_dict_to_np_dict(dataset_dict: dict[str, ad.AnnData]) -> dict[str, np.ndarray]:
     """
     Convert each dataset in the given dictionary from `ad.AnnData` to `np.ndarray`,
     encode the 'cell_type' stored in `adata.obs['cell.type']` as numeric labels,
@@ -54,9 +73,7 @@ def convert_dataset_dict_to_np_dict(
         encoded_values = label_encoder.fit_transform(adata.obs["cell.type"])
 
         # Append encoded values to the data
-        np_dict[dataset_name] = np.concatenate(
-            (np.array(adata.X), encoded_values[:, None]), axis=1
-        )
+        np_dict[dataset_name] = np.concatenate((np.array(adata.X), encoded_values[:, None]), axis=1)
 
     # Sort and return the transformed dictionary
     return dict(sorted(np_dict.items()))
